59a739a8164afe5d727b7f14c0d0aa315e40e62b
Samuel Hess <sam.hess@me.com>
2021-02-14 19:33:19 +0100

    update vue3

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# manager
+# frontend
 
 ## Project setup
 ```
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).mount('#app')
